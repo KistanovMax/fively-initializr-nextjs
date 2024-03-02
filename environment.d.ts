cd2265bde4f6046afe7b121e252474fba4a4e7cd
Maxim Kistanov <maksim.k@5ly.co>
2024-03-02 09:35:08 +0300

    update env

--- environment.d.ts
+++ environment.d.ts
@@ -5,9 +5,5 @@ declare namespace NodeJS {
     NEXTAUTH_SECRET: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
-    AWS_S3_REGION: string;
-    AWS_S3_BUCKET: string;
-    AWS_S3_ACCESS_KEY_ID: string;
-    AWS_S3_SECRET_ACCESS_KEY: string;
   }
 }
